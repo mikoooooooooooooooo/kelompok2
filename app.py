2d253a87d05a24bc40f8bbe0432bea3ebc6cdd10
MIKO <moehherry013@gmail.com>
2025-12-15 01:29:29 +0700

    edit app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 pages = [
-    st.Page(page="pages/page1.py", title="Pendahuluan", icon="📝"),
-    st.Page(page="pages/page2.py", title="Pembahasan", icon="📝"),
-    st.Page(page="pages/page3.py", title="Simpulan dan Saran", icon="📝"),
-    st.Page(page="pages/page4.py", title="Data World Bank", icon="🏦"),
+    st.Page(page="pages/page1.py", title="Pendahuluan (Alma)", icon="📝"),
+    st.Page(page="pages/page2.py", title="Pembahasan (Jess)", icon="📝"),
+    st.Page(page="pages/page3.py", title="Simpulan dan Saran (Chick)", icon="📝"),
+    st.Page(page="pages/page4.py", title="Data World Bank (MixX)", icon="🏦"),
 ]
 
 pg = st.navigation(
